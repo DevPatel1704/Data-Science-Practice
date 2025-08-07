17bdaf6dca0554a97cc96e4157dcc97210b5d71b
DevPatel1704 <pateldev9981@gmail.com>
2025-08-07 10:01:35 +0530

    Dynamic URL generation

--- Flask Framework/jinja.py
+++ Flask Framework/jinja.py
@@ -8,7 +8,7 @@
 '''
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 ### WSGI application
 app = Flask(__name__)
@@ -60,6 +60,25 @@ def successres(score):
 def successif(score):
     
     return render_template('result.html',results=score)
+
+@app.route('/fail/<int:score>')
+def success(score):
+        
+    return render_template('result.html',results=score)
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    total_score=0
+    if request.method == 'POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+        
+        total_score=(science+maths+c+data_science)/4
+        return redirect(url_for('successres',score=total_score))
+    return render_template('get_result.html')
+
 
 if __name__== "__main__":
     app.run(debug=True)
