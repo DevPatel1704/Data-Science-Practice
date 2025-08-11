ff0025a290ce7ee0015a2e9d7be455233d574173
DevPatel1704 <pateldev9981@gmail.com>
2025-08-11 17:05:38 +0530

    Extracting json data

--- Python Prostack/readjson2.py
+++ Python Prostack/readjson2.py
@@ -5,3 +5,9 @@ fp = open('emp1.json','r')
 employee = json.load(fp)
 
 print(employee)
+
+## Want to print only male employes
+
+for emp in employee:
+    if emp['gender'] == "Male":
+        print("Employee Id",emp['eid'],"and emp of Name:",emp['name'])
