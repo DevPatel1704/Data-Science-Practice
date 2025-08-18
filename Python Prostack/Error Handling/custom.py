77ee025b1cd020efb5d4fbf5e9eb0d6184339e56
DevPatel1704 <pateldev9981@gmail.com>
2025-08-18 16:14:55 +0530

    raise

--- Python Prostack/Error Handling/custom.py
+++ Python Prostack/Error Handling/custom.py
@@ -1,1 +1,21 @@
-print(10/0)
+class Inbalance(Exception):
+    def __init__(self,msg):
+        print("inside constructor")
+        self.msg = msg
+        
+      
+def withdrawl(amount):
+    bal = 500
+    if bal>amount:
+        print("take it")
+    else:
+        # print("not enough balance")
+        try:
+            raise Inbalance("balance is low ")
+        except Inbalance as err:
+            print("Inside exception")
+            print(err)
+    
+withdrawl(5000)
+
+
